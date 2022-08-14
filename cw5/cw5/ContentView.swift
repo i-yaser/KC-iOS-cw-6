30ce50fd7c9e7f1a236909a1b5a7a65a3d9e79fe
ياسر حمد العجمي <109863263+i-yaser@users.noreply.github.com>
2022-08-14 17:28:12 +0300

    OURname

--- cw5/cw5/ContentView.swift
+++ cw5/cw5/ContentView.swift
@@ -10,56 +10,53 @@ import SwiftUI
 struct ContentView: View {
     @State var counter: [Int] = [0, 0, 0]
     var body: some View {
-        VStack{
-            HStack{
-                Text("أستغفر الله العظيم").font(.title)
-                Spacer()
-                Text("\(counter[0])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[0] = counter[0] + 1
-                    }
-            }.padding()
-            HStack{
-                Spacer()
-                Text("الحمدلله").font(.title)
-                Spacer()
-                Text("\(counter[1])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[1] = counter[1] + 1
-                    }
-            }.padding()
-            HStack{
-                Text("سبحان الله وبحمده").font(.title)
-                Spacer()
-                Text("\(counter[2])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[2] = counter[2] + 1
-                    }
-            }.padding()
-        }
+
+            VStack{
+                OurName(tap0: $counter[0], name: "أستغرالله العظيم")
+                OurName(tap0: $counter[1], name: "الحمدلله")
+                OurName(tap0: $counter[2], name: "سبحان الله")
+            }
+        //نهاية ال VStack
+        
+            .frame(width: 420, height: 900) .background(.teal)
+            
+        
     }
 }
 
 
 // يجب عمل extract
+
+
+
+struct OurName: View {
+    @Binding var tap0 : Int
+    @State var name : String
+    var body: some View {
+
+        HStack{
+        
+            
+            Text(name).font(.title)
+                .foregroundColor(.white)
+                .background(Color.black)
+                
+            
+            Spacer()
+            Text("\(tap0)")
+                .font(.largeTitle)
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(.white)
+                .background(Color.blue) .opacity(0.9)
+                .clipShape(Capsule())
+                .padding()
+                .onTapGesture {
+                    tap0 = tap0 + 1
+                }
+        }.padding()
+    }
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
